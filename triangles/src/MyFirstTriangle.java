aa660e2ca1b81b4374ee8722f7db3113b8d7c339
Moritz Meisel <moritz@eummeisel.local>
2017-04-10 17:12:35 +0200

    iso triangle

--- triangles/src/MyFirstTriangle.java
+++ triangles/src/MyFirstTriangle.java
@@ -3,19 +3,37 @@
  */
 public class MyFirstTriangle {
     public static void main(String[]  args) {
-        printHorizontalLine(8);
+        System.out.println("Print horizontal line");
+        printHorizontalLine(0, 8);
+        System.out.println("Print vertical line");
         printVerticalLine(3);
+        System.out.println("Print triangle line");
         printTriangle(3);
+        System.out.println("Print iso triangle line");
+        printIsoTriangle(3);
     }
 
-    private static void printHorizontalLine(int number) {
+    private static void printHorizontalLine(int spaces, int number) {
         int x = number;
+
+        System.out.print(getSpaces(spaces));
+
         while (x > 0) {
             System.out.print("*");
             x -= 1;
         }
         System.out.print("\n");
+    }
 
+
+    private static String getSpaces(int count) {
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < count; i++) {
+            sb.append(" ");
+        }
+
+        return sb.toString();
     }
 
     private static void printVerticalLine(int number) {
@@ -28,7 +46,18 @@ public class MyFirstTriangle {
 
     private static void printTriangle(int number) {
         for(int i = 0; i < number; i++) {
-            printHorizontalLine(i+1);
+            printHorizontalLine(0, i+1);
+        }
+    }
+
+    private static void printIsoTriangle(int lines) {
+        int l = lines;
+        int count = 1;
+
+        while(l > 0) {
+            printHorizontalLine(l - 1, count);
+            l--;
+            count += 2;
         }
     }
 
