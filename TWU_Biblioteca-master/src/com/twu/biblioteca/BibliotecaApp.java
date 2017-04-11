6451f887032fcc6a852e2be7492829288bead89b
Moritz Meisel <mmeisel@thoughtworks.com>
2017-04-11 14:30:01 +0200

    user greeting

--- TWU_Biblioteca-master/src/com/twu/biblioteca/BibliotecaApp.java
+++ TWU_Biblioteca-master/src/com/twu/biblioteca/BibliotecaApp.java
@@ -3,6 +3,14 @@ package com.twu.biblioteca;
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        new BibliotecaApp().start();
+    }
+
+    private void start() {
+        greetUser();
+    }
+
+    private void greetUser() {
+        System.out.println("Hello user welcome to the Bibliotheka App");
     }
 }
--- TWU_Biblioteca-master/test/com/twu/biblioteca/ExampleTest.java
+++ TWU_Biblioteca-master/test/com/twu/biblioteca/ExampleTest.java
@@ -1,13 +1,28 @@
 package com.twu.biblioteca;
 
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
 
+    private OutputStream out;
+
+    @Before
+    public void setUp() {
+        out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+    }
+
     @Test
-    public void test() {
-        assertEquals(1, 1);
+    public void testGreeting() {
+        BibliotecaApp.main(new String[] {});
+        assertEquals("Hello user welcome to the Bibliotheka App\n", out.toString());
     }
 }
